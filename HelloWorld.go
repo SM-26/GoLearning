0d85118b08bdd07ebfd4c01632206732d8dd1654
SM26 <39513660+SM-26@users.noreply.github.com>
2021-10-14 23:03:46 +0300

    Giving up on splash screen

--- HelloWorld.go
+++ HelloWorld.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"log"
-	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
-	"fyne.io/fyne/v2/driver/desktop"
 )
 
 func main() {
 
 	myApp := app.New()
+	
 	myWindow := myApp.NewWindow("Hello")
+	
+	icon, iconErr := fyne.LoadResourceFromPath(".\\Resources\\RSLicon.png")
+
+	if iconErr != nil {
+		log.Fatalln("Error in icon loading")
+	}
+	myWindow.SetIcon(icon)
 	myWindow.SetContent(widget.NewLabel("Hello, World!"))
 	myWindow.CenterOnScreen()
 	myWindow.Resize(fyne.NewSize(550, 550))
@@ -29,24 +35,9 @@ func main() {
 	tabs.Append(container.NewTabItemWithIcon("About", theme.HelpIcon(), widget.NewLabel("here is what we got so far")))
 
 	tabs.SetTabLocation(container.TabLocationTop)
-	icon, iconErr := fyne.LoadResourceFromPath(".\\Resources\\RSLicon.png")
-
-	if iconErr != nil {
-		log.Fatalln("Error in icon loading")
-	}
+	
 	myWindow.SetContent(tabs)
-	myWindow.SetIcon(icon)
-
-	drv := fyne.CurrentApp().Driver()
-	w := drv.CreateSplashWindow()
-				w.SetContent(widget.NewLabelWithStyle("Super early alpha\n\nYou have been warned!",
-					fyne.TextAlignCenter, fyne.TextStyle{Bold: true}))
-				w.Show()
-
-				go func() {
-					time.Sleep(time.Second * 3)
-					w.Close()
-				}()
-
 	myWindow.ShowAndRun()
+
+	
 }
