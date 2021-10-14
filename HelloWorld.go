2748a3bb2f70dc9c3216d40a247521ad89e0cdab
SM26 <39513660+SM-26@users.noreply.github.com>
2021-10-14 22:06:28 +0300

    Testing commit

--- HelloWorld.go
+++ HelloWorld.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	//"log"
+	"log"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	//"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
-	//"fyne.io/fyne/v2/canvas"
 )
 
 func main() {
@@ -17,21 +16,24 @@ func main() {
 	myWindow := myApp.NewWindow("Hello")
 	myWindow.SetContent(widget.NewLabel("Hello, World!"))
 	myWindow.CenterOnScreen()
-	myWindow.Resize(fyne.NewSize(550,550))
+	myWindow.Resize(fyne.NewSize(550, 550))
 
 	tabs := container.NewAppTabs(
-		container.NewTabItemWithIcon("Sideloader",theme.HomeIcon(), widget.NewLabel("Hello")),
-		container.NewTabItemWithIcon("Games",theme.ListIcon(), widget.NewLabel("World!")),
-		)
+		container.NewTabItemWithIcon("Sideloader", theme.HomeIcon(), widget.NewLabel("Hello")),
+		container.NewTabItemWithIcon("Games", theme.ListIcon(), widget.NewLabel("World!")),
+	)
 
 	tabs.Append(container.NewTabItemWithIcon("Settings", theme.SettingsIcon(), widget.NewLabel("Settings Text")))
 	tabs.Append(container.NewTabItemWithIcon("About", theme.HelpIcon(), widget.NewLabel("here is what we got so far")))
 
 	tabs.SetTabLocation(container.TabLocationTop)
-	
+	icon, iconErr := fyne.LoadResourceFromPath(".\\Resources\\RSLicon.png")
 
+	if iconErr != nil {
+		log.Fatalln("Error in icon loading")
+	}
 	myWindow.SetContent(tabs)
-	myWindow.SetIcon(resourceResourcesRSLiconPng)
+	myWindow.SetIcon(icon)
 
 	myWindow.ShowAndRun()
 }
