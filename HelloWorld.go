acd39d6efe795ba91f5511c2daded261b82d4a72
SM-26 <39513660+SM-26@users.noreply.github.com>
2021-10-14 15:15:08 +0300

    Tabbs and Icons

--- HelloWorld.go
+++ HelloWorld.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	//"log"
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	//"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -10,7 +15,20 @@ func main() {
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Hello")
 	myWindow.SetContent(widget.NewLabel("Hello, World!"))
-	// fmt.Println("Test line")
+	myWindow.CenterOnScreen()
+	myWindow.Resize(fyne.NewSize(550,550))
+
+	tabs := container.NewAppTabs(
+		container.NewTabItemWithIcon("Sideloader",theme.HomeIcon(), widget.NewLabel("Hello")),
+		container.NewTabItemWithIcon("Games",theme.ListIcon(), widget.NewLabel("World!")),
+		)
+
+	tabs.Append(container.NewTabItemWithIcon("Settings", theme.SettingsIcon(), widget.NewLabel("Settings Text")))
+	tabs.Append(container.NewTabItemWithIcon("About", theme.HelpIcon(), widget.NewLabel("here is what we got so far")))
+
+	tabs.SetTabLocation(container.TabLocationTop)
+
+	myWindow.SetContent(tabs)
 
 	myWindow.ShowAndRun()
 }
