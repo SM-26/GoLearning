5dce6b0f3f215bbe80a9e96b8eed1af97bbdf1bb
SM-26 <39513660+SM-26@users.noreply.github.com>
2021-10-18 14:11:42 +0300

    pritty printer

--- HelloWorld.go
+++ HelloWorld.go
@@ -5,6 +5,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+
 	//"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
@@ -17,18 +18,17 @@ func main() {
 	myWindow := myApp.NewWindow("Hello")
 	myWindow.SetContent(widget.NewLabel("Hello, World!"))
 	myWindow.CenterOnScreen()
-	myWindow.Resize(fyne.NewSize(550,550))
+	myWindow.Resize(fyne.NewSize(550, 550))
 
 	tabs := container.NewAppTabs(
-		container.NewTabItemWithIcon("Sideloader",theme.HomeIcon(), widget.NewLabel("Hello")),
-		container.NewTabItemWithIcon("Games",theme.ListIcon(), widget.NewLabel("World!")),
-		)
+		container.NewTabItemWithIcon("Sideloader", theme.HomeIcon(), widget.NewLabel("Hello")),
+		container.NewTabItemWithIcon("Games", theme.ListIcon(), widget.NewLabel("World!")),
+	)
 
 	tabs.Append(container.NewTabItemWithIcon("Settings", theme.SettingsIcon(), widget.NewLabel("Settings Text")))
 	tabs.Append(container.NewTabItemWithIcon("About", theme.HelpIcon(), widget.NewLabel("here is what we got so far")))
 
 	tabs.SetTabLocation(container.TabLocationTop)
-	
 
 	myWindow.SetContent(tabs)
 	myWindow.SetIcon(resourceResourcesRSLiconPng)
