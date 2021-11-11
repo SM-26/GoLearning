b02f307d5b953fb4abc73c82f58281316e820fb1
SM-26 <39513660+SM-26@users.noreply.github.com>
2021-11-11 13:35:51 +0200

    I18n testings

--- HelloWorld.go
+++ HelloWorld.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -10,16 +11,19 @@ import (
 	//"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+
+	_ "github.com/sm-26/GoLearning/translations"
 )
 
 func main() {
 
 	myApp := app.New()
-	
 	myWindow := myApp.NewWindow("Hello")
 	
 	icon, iconErr := fyne.LoadResourceFromPath(".\\Resources\\RSLicon.png")
-
 	if iconErr != nil {
 		log.Fatalln("Error in icon loading")
 	}
@@ -27,13 +31,39 @@ func main() {
 	myWindow.SetContent(widget.NewLabel("Hello, World!"))
 	myWindow.CenterOnScreen()
 	myWindow.Resize(fyne.NewSize(550, 550))
+	
+	// i18n default setup
+	local := "en-gb"
+	// Declare variable to hold the target language tag.
+	var lang language.Tag
+
+	switch local{
+	case "en-gb":
+		lang = language.MustParse("en-GB")
+		lang = language.BritishEnglish
+	case "en-us":
+		//lang = language.MustParse("en-US")
+		lang = language.AmericanEnglish
+	case "he-il":
+		//lang = language.MustParse("he-IL")
+		lang = language.Hebrew
+		
+	default:
+		log.Fatalln("Local not found")
+		return
+	}
+
+	 // Initialize a message.Printer which uses the target language.
+	 p := message.NewPrinter(lang)
+	 // Print the welcome message translated into the target language.
+	 p.Fprintf(os.Stdout,"The set local is %s\n",lang)
 
 	tabs := container.NewAppTabs(
 		container.NewTabItemWithIcon("Sideloader", theme.HomeIcon(), widget.NewLabel("Hello")),
 		container.NewTabItemWithIcon("Games", theme.ListIcon(), widget.NewLabel("World!")),
 	)
 
-	tabs.Append(container.NewTabItemWithIcon("Settings", theme.SettingsIcon(), widget.NewLabel("Settings Text")))
+	tabs.Append(container.NewTabItemWithIcon("Settings", theme.SettingsIcon(),	widget.NewButton("Set Hebrew", func() {lang = language.Hebrew})))
 	tabs.Append(container.NewTabItemWithIcon("About", theme.HelpIcon(), widget.NewLabel("here is what we got so far")))
 
 	tabs.SetTabLocation(container.TabLocationTop)
---
+++ translations/translations.go
@@ -0,0 +1,3 @@
+package translations
+
+//go:generate gotext -srclang=en-GB update -out=catalog.go -lang=en-GB,he-IL,en-US github.com/sm-26/GoLearning
